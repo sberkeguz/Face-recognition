665c7e0bb7ec7dfc2b70ef281ebf262f43288a26
sberkeguz <sberkeguz@gmail.com>
2026-05-11 16:33:05 +0300

    Temel Dosyalar eklendi.

---
+++ VeriToplama.java
@@ -0,0 +1,5 @@
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.videoio.VideoCapture;
