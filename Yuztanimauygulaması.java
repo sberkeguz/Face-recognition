30e6a04d7a34ea42ef4ba2f1fb5019ef89232722
sberkeguz <sberkeguz@gmail.com>
2026-05-28 18:25:30 +0300

    değişiklik

--- Yuztanimauygulaması.java
+++ Yuztanimauygulaması.java
@@ -4,3 +4,21 @@ import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 
+public class Yuztanimauygulamasi extends JFrame {
+
+    private JLabel KameraEkrani;
+
+    public Yuztanimauygulamasi(){
+        //Opencv ekleme
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        //Pencere ayarıları
+        setTitle("Yüz Tanıma Ve Veri Toplama");
+        setSize(800,600);
+        //uygulamayı kapatmak
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //ekranın ortasına açış
+        setLocationRelativeTo(null);
+        
+    }
+}
