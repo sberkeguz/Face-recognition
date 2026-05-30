26d583758890808d3ff58fcb22edcf3b7d27dd03
sberkeguz <sberkeguz@gmail.com>
2026-05-30 18:07:33 +0300

    çabalıyorum

--- Yuztanimauygulaması.java
+++ Yuztanimauygulaması.java
@@ -1,24 +1,40 @@
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 
 public class Yuztanimauygulamasi extends JFrame {
 
-    private JLabel KameraEkrani;
+    private JLabel kameraEkrani;
+    //kamera 
+    private VideoCapture camera; 
+    //kamera durumu
+    private boolean kameraCalisiyor = false; 
 
     public Yuztanimauygulamasi(){
         //Opencv ekleme
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
         //Pencere ayarıları
-        setTitle("Yüz Tanıma Ve Veri Toplama");
+        setTitle("Yüz Tanıma ve Veri Toplama");
         setSize(800,600);
         //uygulamayı kapatmak
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //ekranın ortasına açış
         setLocationRelativeTo(null);
         
+        //SingsConstants.center ===> metni ortaya yazmak için
+        kameraEkrani = ne JLabel("Kamera başlatılamd",SwingConstants.CENTER)
+        
+        add(kameraEkrani);
+
+        //kamerayı açmak 
+        KamerayiBaslat();
+    }
+
+    public void KamerayiBaslat() {
+        camera = new
     }
 }
